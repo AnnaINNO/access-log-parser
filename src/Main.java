22e407526c05b0b519d293439ec28513fd0754e8
vasilevaannagennadevna <annavas91@bk.ru>
2024-09-22 21:54:06 +0300

    added new feature: 1.  counting line length

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ") ;
-                String text = new Scanner(System.in).nextLine() ;
-        System.out.println("Длина текста: " + text.length() );
+        System.out.println("Введите число:");
+        int number = new Scanner(System.in).nextInt();
+        int firstNumber = 5;
+        int secondNumber = 10;
+        double quotient = (double) firstNumber / secondNumber;
     }
 }
