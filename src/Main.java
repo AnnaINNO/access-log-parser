3baadb00206471be58c59c0acc7bb94ba283c59e
vasilevaannagennadevna <annavas91@bk.ru>
2024-09-23 22:48:36 +0300

    added functional ( summ, diff, mult,quotient)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ") ;
-                String text = new Scanner(System.in).nextLine() ;
-        System.out.println("Длина текста: " + text.length() );
+        System.out.println("Введите число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int summ = firstNumber+secondNumber;
+        int diff = firstNumber-secondNumber;
+        int mult = firstNumber*secondNumber;
+
+        double quotient = (double) firstNumber / secondNumber;
+
+        System.out.println(summ);
+        System.out.println(diff);
+        System.out.println(mult);
+        System.out.println(quotient);
+
     }
 }
